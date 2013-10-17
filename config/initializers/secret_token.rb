0d2298d599f33ee01327efc27347c4916298eb6d
Richard Campbell <betam4x@gmail.com>
2013-10-16 20:07:38 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FancyboxExample::Application.config.secret_key_base = 'aa8b55cdcd6f62e586e68ebcc8a8e5e81f3af071884f794b7c5599725415515c0e06c82f3c7f8b0ccf164941adc6db32e3cb713968fca2af6e1892fba9e2890e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FancyboxExample::Application.config.session_store :cookie_store, key: '_FancyboxExample_session'
